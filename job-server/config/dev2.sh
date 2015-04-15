b4aa1ebc20be7d8aa87209f2cc99c40210dc907a
eric liu <eric@vlognow.com>
2015-04-14 22:25:53 -0700

    add new setting for second spark job server

---
+++ job-server/config/dev2.sh
@@ -0,0 +1,14 @@
+# Environment and deploy file
+# For use with bin/server_deploy, bin/server_package etc.
+DEPLOY_HOSTS="172.30.1.188"
+APP_USER=ubuntu
+APP_GROUP=ubuntu
+# optional SSH Key to login to deploy server
+#SSH_KEY=/path/to/keyfile.pem
+INSTALL_DIR=/home/ubuntu/job-server
+LOG_DIR=/mnt/logs/job-server
+PIDFILE=spark-jobserver.pid
+SPARK_HOME=/home/ubuntu/sparkInternalClone
+SPARK_CONF_DIR=$SPARK_HOME/conf
+# Only needed for Mesos deploys
+#SPARK_EXECUTOR_URI=/home/spark/spark-0.8.0.tar.gz
